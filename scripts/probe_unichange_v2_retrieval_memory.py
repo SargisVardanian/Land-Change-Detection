190633236802ad6b3780360df432c8e95d0bd3b9
SargisVardanian <105112007+SargisVardanian@users.noreply.github.com>
2026-07-01 17:58:08 +0400

    Propagate memory probe result

--- scripts/probe_unichange_v2_retrieval_memory.py
+++ scripts/probe_unichange_v2_retrieval_memory.py
@@ -2,4 +2,4 @@ import sys
 from pathlib import Path
 from ucv2_memory_core import run
 
-run(Path(sys.argv[1]), Path(sys.argv[2]), Path(sys.argv[3]), Path(sys.argv[4]), Path(sys.argv[5]))
+assert run(Path(sys.argv[1]), Path(sys.argv[2]), Path(sys.argv[3]), Path(sys.argv[4]), Path(sys.argv[5])) == 0
