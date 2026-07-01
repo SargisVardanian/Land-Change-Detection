c522a90aa47efcc4673e3cc9c1e0c53df37492bf
SargisVardanian <105112007+SargisVardanian@users.noreply.github.com>
2026-07-01 17:50:20 +0400

    Add full retrieval trainer wrapper

---
+++ scripts/train_unichange_v2_retrieval_full.py
@@ -0,0 +1,6 @@
+import sys
+from pathlib import Path
+from ucv2_full_core import run
+
+resume = Path(sys.argv[9]) if len(sys.argv) > 9 and sys.argv[9] else None
+run(Path(sys.argv[1]), Path(sys.argv[2]), Path(sys.argv[3]), Path(sys.argv[4]), Path(sys.argv[5]), int(sys.argv[6]), int(sys.argv[7]), int(sys.argv[8]), resume)
